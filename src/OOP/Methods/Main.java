248e01b394c4caacf672feb26169069e99bb68ab
JackieWeckie <nekit4568i@gmail.com>
2024-10-30 23:38:27 +0300

    OOP Methods practice tasks

---
+++ src/OOP/Methods/Main.java
@@ -0,0 +1,31 @@
+package OOP.Methods;
+
+public class Main {
+    public static void main(String[] args) {
+        methodGetStraightLine();
+        methodGetEvenNum();
+        methodGetMaxNum();
+        methodGetNumRange();
+        methodGetSimpleNum();
+    }
+
+    static void methodGetStraightLine() {
+        System.out.println(methodGetStraightLine.getLine());
+    }
+
+    static void methodGetEvenNum() {
+        System.out.println(methodGetEvenNum.getEvenNum());
+    }
+
+    static void methodGetMaxNum() {
+        System.out.println(methodGetMaxNum.getMaxNum());
+    }
+
+    static void methodGetNumRange() {
+        System.out.println(methodGetNumberRange.getNumRange());
+    }
+
+    static void methodGetSimpleNum() {
+        System.out.println(methodGetSimpleNum.getSimpleNum());
+    }
+}
